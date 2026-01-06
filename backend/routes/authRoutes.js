396a1617eb6d768776a0d67b52715eca2858e599
sanj4git <arsanjay0906@gmail.com>
2026-01-06 15:10:24 +0530

    Add user login API with JWT authentication

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,16 @@
 import express from "express";
-import { registerUser } from "../controllers/authControllers.js";
+import { registerUser, loginUser } from "../controllers/authControllers.js";
 
 const router = express.Router();
 
 // @route POST /api/auth/register
 // @desc Register a new user
 // @access Public
-
 router.post("/register", registerUser);
+
+// @route POST /api/auth/login
+// @desc Existing user login
+// @access Public
+router.post("/login", loginUser);
+
 export default router;
